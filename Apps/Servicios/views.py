9b0bdbb845131994bd69cb41e2223c524ffab82a
AlexRpg-86 <54549313+AlexRpg-86@users.noreply.github.com>
2019-10-16 22:48:31 -0300

    Incluir templates y statics

--- Apps/Servicios/views.py
+++ Apps/Servicios/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 #este es la prueba del primer cambio de alex
+def mostrarReservas(request):
+    return render(request,'Reserva.html',{})
--- Salon/settings.py
+++ Salon/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Apps.Servicios',
+    'Apps.Usuarios',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'Salon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
