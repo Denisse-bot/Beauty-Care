844f5c0837dc2435b8a5c57bfe87edf8a1963e67
Vina del Mar <vina@duoc.acad>
2019-10-17 09:47:38 -0300

    asd

--- Apps/Usuarios/views.py
+++ Apps/Usuarios/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-<<<<<<< HEAD
-#este es la prueba del primer cambio de denisse
-=======
 def mostrarIndex(request):
     return render(request,'Esquema.html',{})
->>>>>>> origin/master
